6fddd793e95a04e791c7d6e3d0cc69134e9565ce
momotaroking <119371383+momo3Dworks@users.noreply.github.com>
2025-06-29 04:07:55 -0300

    6yo

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // IMPORTANTE: Reemplaza esto con tu propia configuración de Firebase
 // desde la configuración de tu proyecto en la consola de Firebase.
 const firebaseConfig = {
-  apiKey: "dasdasd",
+  apiKey: "AIzaSyDeKRqhO6mOi4suJTdoOKMOPPL4ZdiHayw",
   authDomain: "sarvoip-9a4a5.firebaseapp.com",
   projectId: "sarvoip-9a4a5",
   storageBucket: "sarvoip-9a4a5.firebasestorage.app",
   messagingSenderId: "212334592557",
-  appId: "1:212334592557:web:fefa700a1f08f113ef2ea4",
-  measurementId: "G-J3MK94TLNL"
+  appId: "1:212334592557:web:f1ebe458dc26022def2ea4",
+  measurementId: "G-KL71LP7KPE"
 };
 // Inicializar Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
